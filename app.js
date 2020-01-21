49f91d8ea2109afc9134e6627283693e89f12766
Santalov <santalov.stas@gmail.com>
2020-01-21 16:25:42 +0300

    message

--- app.js
+++ app.js
@@ -1,1 +1,51 @@
-navigator.serviceWorker.controller.postMessage('izi');
+async function saveTest() {
+    try {
+        const request = new PaymentRequest([{
+            supportedMethods: 'https://rsolomakhin.github.io/pr/apps/password/',
+            data: {
+                action: 'store',
+                data: 'iziTestMessage',
+            },
+        }], {
+            total: {
+                label: 'N/A',
+                amount: {
+                    currency: 'USD',
+                    value: '0.00'
+                }
+            }
+        });
+        const response = await request.show();
+        document.getElementById('status').innerHTML = 'Username and password stored.';
+        await response.complete('success');
+    } catch (e) {
+        document.getElementById('status').innerHTML = e.toString() + 'Error.';
+    }
+}
+
+async function getTest() {
+    try {
+        document.getElementById('status').innerHTML = 'Getting data';
+        const request = new PaymentRequest([{
+            supportedMethods: 'https://rsolomakhin.github.io/pr/apps/password/',
+            data: {
+                action: 'get',
+            },
+        }], {
+            total: {
+                label: 'N/A',
+                amount: {
+                    currency: 'USD',
+                    value: '0.00'
+                }
+            }
+        });
+        const response = await request.show();
+        if (response.details.data) {
+            document.getElementById('data').innerText = response.details.data;
+        }
+        await response.complete('success');
+    } catch (e) {
+        document.getElementById('status').innerHTML = e.toString() + 'Error.';
+    }
+}
