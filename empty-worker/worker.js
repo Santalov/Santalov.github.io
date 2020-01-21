a9f35986e2b08cf95b2eddc8510192ab32d8a074
Santalov <santalov.stas@gmail.com>
2020-01-21 22:42:24 +0300

    new empty worker interface

---
+++ empty-worker/worker.js
@@ -0,0 +1,15 @@
+self.addEventListener('message', function (event) {
+    if (event.data) {
+
+    } else {
+        console.error('service worker received empty data', event);
+    }
+});
+
+self.addEventListener('paymentrequest', (evt) => {
+    console.log(evt);
+    evt.respondWith(new Promise((resolve) => {
+        console.log('resolve', resolve);
+        evt.openWindow('/form/index.html');
+    }));
+});
