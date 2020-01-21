a101c847e829bf3e9344a98a7bc9feeec6e3db64
Santalov <santalov.stas@gmail.com>
2020-01-21 16:00:31 +0300

    script

--- form.js
+++ form.js
@@ -2,5 +2,5 @@ const parts = window.location.href.split('#');
 
 const data = parts[1];
 const nodata=parts[2];
-console.log(parts);
+console.log('parts', parts);
 document.getElementById('content').innerText = data + nodata;
