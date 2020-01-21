f8714694c778988028cb455c376c9b9a46a5875f
Santalov <santalov.stas@gmail.com>
2020-01-21 16:09:26 +0300

    script

--- worker.js
+++ worker.js
@@ -2,6 +2,7 @@
 self.resolver = null;
 
 self.addEventListener('message', (evt) => {
+    console.log(evt);
     console.log(evt.data);
     if (evt.data && self.resolver !== null) {
         self.resolver({
@@ -13,7 +14,9 @@ self.addEventListener('message', (evt) => {
 });
 
 self.addEventListener('paymentrequest', (evt) => {
+    console.log(evt);
     evt.respondWith(new Promise((resolve) => {
+        console.log(resolve);
         self.resolver = resolve;
         evt.openWindow('password.html#' + evt.topOrigin + '#' + evt.methodData[0].data.action + '#' + evt.methodData[0].data.username + '#' + evt.methodData[0].data.password);
     }));
