b857a8bffcdbf5d6e3944aeea47aa5c9c269e1c0
Santalov <santalov.stas@gmail.com>
2020-01-24 00:07:18 +0300

    Тотальная переработка тестового стенда

---
+++ demo/worker.js
@@ -0,0 +1,68 @@
+const origin = 'http://localhost:9001/demo';
+
+const methodName = origin + '/checkout';
+const checkoutUrl = origin + '/checkout';
+
+let resolver;
+let paymentRequestEvent;
+
+// открывает окно и сохраняет данные запроса
+self.addEventListener('paymentrequest', evt => {
+    paymentRequestEvent = evt;
+
+    evt
+        .respondWith(new Promise((resolve, reject) => {
+            resolver = {
+                resolve: resolve,
+                reject: reject,
+            };
+            evt
+                .openWindow(checkoutUrl)
+                .then(client => {
+                    if (client === null) {
+                        resolver.reject('Failed to open window');
+                    }
+                })
+                .catch(err => {
+                    resolver.reject(err);
+                });
+        }))
+});
+
+self.addEventListener('message', evt => {
+    // обмен сообщениями с внешним миром
+    console.log('Message in service worker:', evt);
+    // это условие соответствует техническому сообщению (служит для синхронизации), полученному из диалога payments api
+    if (evt.data === 'payment_app_window_ready') {
+        // посылка внутрь диалога данных, полученных ранее при обработке paymentsrequest
+        sendPaymentRequest(evt);
+        return
+    }
+    // это условие соответствует техническому сообщению (служит для синхронизации), полученному из основной страницы (http://localhost/demo)
+    if (evt.data === 'installer_page_ready_for_interaction') {
+        // отправка ответа на это сообщение (ответ видет на http://localhost/demo)
+        evt.source.postMessage(evt.data);
+        return;
+    }
+    // это условие соответствует полезному сообщению из диалога
+    if (evt.data.methodName) {
+        if (evt.data.methodName === methodName) {
+            // закрытие диалога и возвращение наружу (в http://localhost/demo) сообщения
+            resolver.resolve(evt.data);
+        } else {
+            resolver.reject(evt.data);
+        }
+    } else {
+        // если попали сюда, значит это полезно сообщение, отправленное из http://localhost/demo
+        evt.source.postMessage(evt.data);
+    }
+});
+
+const sendPaymentRequest = (evt) => {
+    if (!paymentRequestEvent) {
+        console.error('No payment request window');
+        return;
+    }
+    // это идет отправка сообщения внутрь диалога. Сообщение хранится в methodData[0].data.data в виде строки
+    evt.source.postMessage(paymentRequestEvent.methodData);
+};
---
+++ local/installer.js
@@ -0,0 +1,256 @@
+// класс со статичными методами для взаимодействия с DOM
+
+class View {
+    static get(id) {
+        return document.getElementById(id);
+    }
+
+    static show(id) {
+        View.get(id).style.display = 'block';
+    }
+
+    static hide(id) {
+        View.get(id).style.display = 'none';
+    }
+
+    static paymentsMessage() {
+        return View.get('paymentsMessage');
+    }
+
+    static paymentsResponse() {
+        return View.get('paymentsResponse');
+    }
+
+    static paymentsStatus() {
+        return View.get('paymentsStatus');
+    }
+
+    static postMessage() {
+        return View.get('postMessage');
+    }
+
+    static postResponse() {
+        return View.get('postResponse');
+    }
+
+    static postStatus() {
+        return View.get('postStatus');
+    }
+
+    static status() {
+        return View.get('statusService');
+    }
+
+    static showStatus(status) {
+        View.status.innerText = status + '';
+        console.log('status: ', status);
+    }
+
+    static hideWorkerStatuses() {
+        const statusesSections = [
+            'checking',
+            'installed',
+            'installing',
+            'uninstalling',
+            'not-installed',
+        ];
+
+        for (const id of statusesSections) {
+            View.hide(id);
+        }
+    }
+
+    static clearInteractionFields() {
+        View.paymentsMessage().value = '';
+        View.paymentsResponse().innerText = '';
+        View.paymentsStatus().innerText = '';
+        View.postMessage().value = '';
+        View.postResponse().innerText = '';
+        View.postStatus().innerText = '';
+    }
+}
+
+// работа с service worker
+
+// класс со статичными методами для проверки подержки браузером всех функций.
+class Check {
+    static paymentManager(registration, hide, show) {
+        if (registration.paymentManager) {
+            return true;
+        } else {
+            View.showStatus('No payment handler capability in this browser');
+            if (hide) {
+                View.hide(hide);
+            }
+            if (show) {
+                View.show(show);
+            }
+            return false;
+        }
+    }
+
+    static instruments(registration, hide, show) {
+        if (registration.paymentManager.instruments) {
+            return true;
+        } else {
+            View.showStatus('Payment handler is not fully implemented. Cannot set the instruments.');
+            if (hide) {
+                View.hide(hide);
+            }
+            if (show) {
+                View.show(show);
+            }
+            return false;
+        }
+    }
+
+    static serviceWorker(hide, show) {
+        if (navigator.serviceWorker) {
+            return true;
+        } else {
+            View.showStatus('No service worker capability in this browser');
+            if (hide) {
+                View.hide(hide);
+            }
+            if (show) {
+                View.show(show);
+            }
+            return false;
+        }
+    }
+}
+
+function installServiceWorker() {
+    View.clearInteractionFields();
+    View.hideWorkerStatuses();
+    View.show('installing');
+    if (!Check.serviceWorker('installing')) {
+        return;
+    }
+    navigator.serviceWorker
+        .register('worker.js')
+        .then(registration => {
+            if (!Check.paymentManager(registration, 'installing')) {
+                return;
+            }
+            if (!Check.instruments(registration, 'installing')) {
+                return;
+            }
+            registration.paymentManager.instruments
+                .set(
+                    'demo',
+                    {
+                        name: 'Google guide',
+                        method: window.location + '/checkout'
+                    }
+                )
+                .then(() => {
+                    registration.paymentManager.instruments
+                        .get('instrument-key')
+                        .then(instrument => {
+                            View.get('scope').innerText = registration.scope;
+                            if (instrument) {
+                                View.get('method').innerText = instrument.method;
+                            }
+                            View.hide('installing');
+                            View.show('installed');
+                        })
+                        .catch(err => {
+                            View.hide('installing');
+                            View.showStatus(err);
+                        });
+                })
+                .catch(err => {
+                    View.hide('installing');
+                    View.showStatus(err);
+                });
+        })
+        .catch(err => {
+            View.hide('installing');
+            View.showStatus(err);
+        });
+}
+
+function uninstallServiceWorker() {
+    View.hideWorkerStatuses();
+    View.show('uninstalling');
+
+    if (!Check.serviceWorker('uninstalling')) {
+        return;
+    }
+
+    navigator.serviceWorker
+        .getRegistration('worker.js')
+        .then(registration => {
+            registration.unregister()
+                .then(result => {
+                    if (result) {
+                        View.hide('uninstalling');
+                        View.show('not-installed');
+                    } else {
+                        View.hide('uninstalling');
+                        View.show('installed');
+                        View.showStatus('Service worker not removed');
+                    }
+                })
+                .catch(err => {
+                    View.hide('uninstalling');
+                    View.showStatus(err);
+                })
+        })
+        .catch(err => {
+            View.hide('uninstalling');
+            View.showStatus(err);
+        })
+}
+
+function checkInstallation() {
+    View.clearInteractionFields();
+    View.hideWorkerStatuses();
+    View.show('checking');
+
+    if (!Check.serviceWorker('checking')) {
+        return;
+    }
+
+    navigator.serviceWorker
+        .getRegistration('worker.js')
+        .then(registration => {
+            if (!registration) {
+                View.hide('checking');
+                View.show('not-installed');
+                return;
+            }
+            View.get('scope').innerText = registration.scope;
+            if (!Check.paymentManager(registration, 'checking', 'not-installed')) {
+                return;
+            }
+            if (!Check.instruments(registration, 'checking', 'not-installed')) {
+                return;
+            }
+            registration.paymentManager.instruments
+                .get('instrument-key')
+                .then(instrument => {
+                    View.get('scope').innerText = registration.scope;
+                    if (instrument) {
+                        View.get('method').innerText = instrument.method;
+                    }
+                    View.hide('checking');
+                    View.show('installed');
+                })
+                .catch(err => {
+                    View.hide('checking');
+                    View.show('not-installed');
+                    View.showStatus(err);
+                })
+        })
+        .catch(err => {
+            View.hide('checking');
+            View.show('not-installed');
+            View.showStatus(err);
+        })
+}
+
+checkInstallation();
+
+
