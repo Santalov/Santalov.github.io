7f2e594895898cd598ef1f27db0cee0a179a90ae
Santalov <santalov.stas@gmail.com>
2020-01-30 12:37:32 +0300

    В local кешируется диалог checkout

--- local/interaction.js
+++ local/interaction.js
@@ -41,7 +41,7 @@ function openWindow() {
         const msg = View.paymentsMessage().value;
         View.paymentsMessage().value = '';
         const request = new PaymentRequest([{
-            supportedMethods: window.location + 'checkout',
+            supportedMethods: window.location + '/checkout',
             data: {
                 data: msg
             }
