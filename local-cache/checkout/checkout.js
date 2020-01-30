b130f42772322c610a2282e057f9dbdfaf3eabcc
Santalov <santalov.stas@gmail.com>
2020-01-30 14:06:05 +0300

    Версия local с кэшем вынесена отдельно

---
+++ local-cache/checkout/checkout.js
@@ -0,0 +1,25 @@
+const origin = 'http://localhost:9001/local-cache';
+const methodName = origin + '/checkout';
+
+let client;
+let firstMsg;
+
+navigator.serviceWorker.addEventListener('message', e => {
+    firstMsg = e.data[0].data.data;
+    document.getElementById('response').innerText = firstMsg;
+    client = e.source;
+});
+navigator.serviceWorker.controller.postMessage('payment_app_window_ready');
+
+function send() {
+    const msg = document.getElementById('textarea').value;
+    if (!client) return;
+    const msgObject = {
+        methodName: methodName,
+        details: {
+            firstMsg: firstMsg,
+            secondMsg: msg,
+        }
+    };
+    client.postMessage(msgObject);
+}
